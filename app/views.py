967aeec0559bca5b05bcc46bc451ba208897d96d
othienoJoe <bryo094@gmail.com>
2021-09-10 12:35:30 +0300

    Structured the views.py file

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from flask import render_template
+from app import app
+
+# The views
+@app.route('./')
+def index():
+	"""
+	This is the view root page that returns the index page and its data.
+	"""
+	return render_template('index.html')
