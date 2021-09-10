be37a7ca7a9da5de5f01f768e407198db1259b88
othienoJoe <bryo094@gmail.com>
2021-09-10 14:02:19 +0300

    reviewed views.py and __init__.py files to the required structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 #Let's Initialize the application
 app = Flask(__name__)
+
+#Configuration Set Up.
+app.config.from_object(DevConfig)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -7,8 +7,8 @@ def index():
 	"""
 	This is the view root page that returns the index page and its data.
 	"""
-	message = 'Hello World'
-	return render_template('index.html', message = message)
+	title = 'Home - Welcome to Number 0ne(1) News Site'
+	return render_template('index.html', title = title)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
@@ -16,4 +16,3 @@ def news(news_id):
 	A function for the news that returns the news' details and it data.
 	"""
 	return render_template('news.html',id = news_id)
-	
